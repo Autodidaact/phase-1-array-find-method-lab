3c6286878ddc57e4b6fb873386e006af80a35f70
Ibrahim Abdulai Yakubu <ibrahimay295@gmail.com>
2022-06-15 11:10:44 +0000

    Modified index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function superbowlWin(array){
+    let ifwon = (draw) => {
+        return (draw.result == 'W')
+        }
+       
+    const state = array.find(ifwon);
+    if (state === undefined){
+        return undefined;
+    }else{
+        return state.year;
+    }
+   
+}
+
+
